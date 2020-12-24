475e0c46b6114f90c71cabb10b8cb3ce1ef9efdd
shannu <yourshannu1@gmail.com>
2020-12-24 13:21:58 +0530

    Initail Commit

---
+++ EurekaServerConfig/src/main/java/com/sankar/tech/pro/EurekaServerConfigApplication.java
@@ -0,0 +1,15 @@
+package com.sankar.tech.pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerConfigApplication.class, args);
+	}
+
+}
